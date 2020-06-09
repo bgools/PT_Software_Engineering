802a438171ca9513ec5cf43832351d351e6bf074
Bryan Goolsby <bgoolz@bgoolzpductions.home>
2020-06-08 23:53:28 -0400

    Wild Wild Life Completed

--- Mod1/week-2/w2d2/homework/answers.js
+++ Mod1/week-2/w2d2/homework/answers.js
@@ -1,3 +1,4 @@
+/*
 //Loop numbers 1 - 20
 for (i=1; i<=20; i++){
     console.log(i);
@@ -13,3 +14,40 @@ for (i=0; i<=200; i+=2){
 //////////////////////
 //Get Even Answered///
 //////////////////////
+
+//FizzBuzz///
+*/
+////Wild Wild Life////
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+plantee[2] = 5001;
+wolfy[3] = "Gothem City";
+dart.push ("Hawkins");
+wolfy[0] = "Gameboy";
+
+console.log(wolfy);
+console.log(sharky);
+console.log(plantee);
+console.log(porgee);
+console.log(dart);
+
+/*
+//Yell at the Ninja Turtles///
+const turles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+for (name of turles){
+    let yellAtTurtles = name.toUpperCase();
+    console.log(yellAtTurtles);
+}
+*/
+
+//Methods, Revisited///
+
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 
+'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 
+'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+console.log(favMovies[8]);
