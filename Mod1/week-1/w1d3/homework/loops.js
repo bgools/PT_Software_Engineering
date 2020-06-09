dea62741811c3c925a8883ebda1d2f9359ed8ca0
Bryan Goolsby <bgoolz@bgoolzpductions.home>
2020-06-08 22:04:10 -0400

    finished loop practice

---
+++ Mod1/week-1/w1d3/homework/loops.js
@@ -0,0 +1,26 @@
+
+// Ascending Loop (made loop ascend by 3)
+for (i=0; i<=99; i+=3){
+    console.log(i);
+}
+// Decending Loop (made loop decend by 3)
+for (i=99; i>=0; i-=3){
+    console.log(i);
+}
+//Even Numbers Ascending
+for (i=0; i<=98; i+=2){
+    console.log(i);
+}
+//Even Numbers Decending
+for (i=98; i>=0; i-=2){
+    console.log(i);
+}
+
+//Ascending Loop from 49-72
+for (i = 49; i<=72; i++){
+    console.log(i)
+}
+//Decending Loop from 81-26
+for (i = 81; i>=26; i--){
+    console.log(i)
+}
