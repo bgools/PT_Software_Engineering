bf6037c6339619de43d8b2c6549621155ab473e0
Bryan Goolsby <bgoolz@bgoolzpductions.home>
2020-06-08 22:20:15 -0400

    Get Even Answered

--- Mod1/week-2/w2d2/homework/answers.js
+++ Mod1/week-2/w2d2/homework/answers.js
@@ -6,3 +6,10 @@ for (i=1; i<=20; i++){
 //Easy Going////////
 ////////////////////
 
+//Even numbers Loop 0-200
+for (i=0; i<=200; i+=2){
+    console.log(i);
+}
+//////////////////////
+//Get Even Answered///
+//////////////////////
