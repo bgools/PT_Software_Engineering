585fe8f844100558651ec84245c3a71fdf59010c
Bryan Goolsby <bgoolz@bgoolzpductions.home>
2020-06-15 18:53:41 -0400

    crazy object complete

--- Mod1/week-3 /w3d3/homework/answers.js
+++ Mod1/week-3 /w3d3/homework/answers.js
@@ -1,4 +1,4 @@
-
+/*
 /////////////////////////
 ///Datatype Refresher////
 /////////////////////////
@@ -24,27 +24,28 @@ const spaceship = ['hull', 'laser blasters', 'tractor beams', 'warp drive']
 const studentNames = ['Antwan Brinkley', 'Arron Aman', 'Beth Mallett', 'Thomas Vinson']
 
 // 5.
-//datatype: Objects
+//datatype: Array of Objects
 //data structure example: 
-/*
+
 const studentNames_Locations = [
     {name: 'Antwan', location: 'Columbus, OH'},
     {name: 'Arron', location: 'Columbus, OH'},
     {name: 'Beth', location: 'Columbus, OH'},
     {name: 'Thomas', location: 'Columbus, OH'}
 ]
+console.log(studentNames_Locations);
 
 // 6.
-//datatype: Array of Object with Array
+//datatype: Array of Objects with Array
 //data structure ezample:
-const studentNames_Locations = [
+const studentNames_Locations1 = [
     {name: 'Antwan', location: 'Columbus, OH', favTvShows: ['Grimm', 'Agents of Shield', 'The Simpsons']},
     {name: 'Arron', location: 'Columbus, OH', favTvShows: ['Bones', 'Smallville', 'Darkwing Duck']},
     {name: 'Beth', location: 'Columbus, OH', favTvShows: ['The Mentalist', 'Flash', 'Family Guy']},
     {name: 'Thomas', location: 'Columbus, OH', favTvShows: ['Betas', 'Silicon Valley', 'Care Bears']},
 ]
-console.log(studentNames_Locations);
-*/
+console.log(studentNames_Locations1);
+
 
 //////////////////////////
 ///Take It Easy///////////
@@ -52,6 +53,7 @@ console.log(studentNames_Locations);
 
 // 1.
 const colorsOfTheRainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
 
 // 2.
 console.log(colorsOfTheRainbow[4]);
@@ -66,5 +68,57 @@ const bryan = {
 
 // 4.
 console.log(bryan.hobby);
+
+*/
+
+////////////////////////
+///Crazy Object!////////
+////////////////////////
+
+const crazyObject = {
+    taco: [{meat: 'steak',
+           cheese: ['panela', 'queso', 'chihuahua']},
+           {meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+          ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [{
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+      ]
+    }
+  }
+
+  // 1.
+  console.log(crazyObject.taco[1].salsa[5]);
+
+  // 2. 
+  console.log(crazyObject.larry.quotes[0]);
+
+  // 3.
+  console.log(crazyObject.larry.characters[2].favourtieHobby);
+
+  // 4.
+  console.log(crazyObject.larry.nicknames[1]);
+
+  // 5.
+  console.log(crazyObject.larry.characters[1]);
+
+  // 6.
+  crazyObject.larry.quotes.push("I\'m trying to elevate small talk to medium talk");
+  console.log(crazyObject.larry.quotes);
+  
 
 
