3de08adb594b8a2d6e8c45f5cf0b4decc01b9b02
Bryan Goolsby <bgoolz@bgoolzpductions.home>
2020-06-15 17:49:50 -0400

    datatype refresher complete

---
+++ Mod1/week-3 /w3d3/homework/answers.js
@@ -0,0 +1,44 @@
+
+/////////////////////////
+///Datatype Refresher////
+/////////////////////////
+
+// 1.
+//datatype: Boolean
+//data structure example: 
+const light = true;
+
+// 2.
+//datatype: string
+//data structure example: 
+let email = "johndoe@email.com";
+
+// 3.
+//datatype: Array
+//data structure example: 
+const spaceship = ['hull', 'laser blasters', 'tractor beams', 'warp drive']
+
+// 4.
+//datatype: Array
+//data structure example: 
+const studentNames = ['Antwan Brinkley', 'Arron Aman', 'Beth Mallett', 'Thomas Vinson']
+
+// 5.
+//datatype: Objects
+//data structure example: 
+const studentNames_Locations = [
+    {name: 'Antwan', location: 'Columbus, OH'}
+    {name: 'Arron', location: 'Columbus, OH'}
+    {name: 'Beth', location: 'Columbus, OH'}
+    {name: 'Thomas', location: 'Columbus, OH'}
+]
+
+// 6.
+//datatype: Array of Object with Array
+//data structure ezample:
+const studentNames_Locations = [
+    {name: 'Antwan', location: 'Columbus, OH', favTvShows: ['Grimm', 'Agents of Shield', 'The Simpsons']}
+    {name: 'Arron', location: 'Columbus, OH', favTvShows: ['Bones', 'Smallville', 'Darkwing Duck']}
+    {name: 'Beth', location: 'Columbus, OH', favTvShows: ['The Mentalist', 'Flash', 'Family Guy']}
+    {name: 'Thomas', location: 'Columbus, OH', favTvShows: ['Betas', 'Silicon Valley', 'Care Bears']}
+]
