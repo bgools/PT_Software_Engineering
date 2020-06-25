50f64e643313cb81c689db8e3f4950c625d442ae
Bryan Goolsby <bgoolz@bgoolzpductions.home>
2020-06-24 20:18:38 -0400

    created Person class

--- Mod1/week-5/w5d1/homework/objects.js
+++ Mod1/week-5/w5d1/homework/objects.js
@@ -4,6 +4,8 @@
 ///Creating Classes///
 //////////////////////
 
+//Hamster//
+
 class Hamster {
     constructor(owner, name, price){
         this.owner = ''
@@ -23,3 +25,64 @@ class Hamster {
 }
 //const bill = new Hamster('Bryan', 'Bill', 20)
 //console.log(bill);
+
+//Person//
+
+class Person {
+    constructor(name, age, height, weight, mood, hamsters, bankAccount){
+        this.name = name
+        this.age = age
+        this.height = height
+        this.weight = weight
+        this.mood = mood
+        this.hamsters = hamsters
+        this.bankAccount = bankAccount
+    }
+    getName(){
+        let name = this.name
+        return name;
+    }
+    getAge(){
+        let age = this.age
+        return age;
+    }
+    getWeight(){
+        let weight = this.weight
+        return weight;
+    }
+    greet(){
+        console.log('Hi, my name is ' + this.name + '!');
+    }
+    eat(){
+        if (this.weight === 10 && this.mood === 10){
+            console.log(++this.weight);
+            console.log(++this.mood);
+        }
+    }
+    exercise(){
+        if (this.weight === 10){
+            console.log(--this.weight);            
+        }
+    }
+    ageUp(){
+        let age = ++this.age
+        console.log(age);
+        let height = ++this.height
+        console.log(height);
+        let weight = ++this.weight
+        console.log(weight);
+        let mood = --this.mood
+        console.log(mood);
+        let bankAccount = (this.bankAccount + 10)
+        console.log(bankAccount);
+        
+    }
+    //buyHamster(hamster)
+
+}
+const bryan = new Person('Bryan', 34, 72, 10, 10, ['bill', 'jack', 'jill'], 300)
+console.log(bryan.getName());
+console.log(bryan.getAge());
+console.log(bryan.getWeight());
+
+
